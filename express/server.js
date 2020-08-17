273227324c5217fffcd51cc0d5b9139b87eadd6d
Kevin Power <kpow@kpow.com>
2020-08-17 16:51:49 -0400

    Update server.js

--- express/server.js
+++ express/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Hello from snoopy.js!</h1>');
   res.end();
 });
 router.get('/kpow/', (req, res) => {
